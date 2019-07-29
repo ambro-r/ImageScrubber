f1a9dc626a30896f873be9c34f9fd0777c4b7d21
ambro-r <rrambrosio@gmail.com>
2019-07-29 15:41:21 +0200

    Initial commit.

---
+++ src/main/java/is/ImageScrubber.java
@@ -0,0 +1,10 @@
+package is;
+
+public class ImageScrubber {
+
+    private final Logger LOG = LoggerFactory.getLogger(ImageScrubber.class);
+
+    public static void main(String[] args) {
+    }
+
+}
